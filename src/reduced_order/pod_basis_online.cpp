365abfa7d57f6d28c43bc793ca27a1ef4e9978f4
tofstie <tysonofstie@gmail.com>
2025-02-21 15:13:23 -0500

    Removed pedantic errors

--- src/reduced_order/pod_basis_online.cpp
+++ src/reduced_order/pod_basis_online.cpp
@@ -146,13 +146,13 @@ MatrixXd OnlinePOD<dim>::getSnapshotMatrix() {
 template <int dim>
 std::shared_ptr<dealii::TrilinosWrappers::SparseMatrix> OnlinePOD<dim>::getSkewSymmetric() {
     return basis;
-};
+}
 
 /// This function is not defined for online basis calculations
 template <int dim>
 MatrixXd OnlinePOD<dim>::getTestBasis() {
     return snapshotMatrix;
-};
+}
 template <int dim>
 
 void OnlinePOD<dim>::CalculateL2Error(std::shared_ptr <dealii::TableHandler> /*L2error_data_table*/,
